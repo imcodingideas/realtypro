5a68e1fddcedc56463cebedb32c358a35ce57443
Joseph Chambers <joseph@michael-chambers.com>
2018-04-08 17:57:22 -0700

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
+    path('profile/<int:user_id>/', views.view_profile, name='view_profile'),
+    path('profile/<int:user_id>/edit', views.edit_profile, name='edit_profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
 
 # Create your views here.
 def signup(request):
   if request.method == 'POST':
-    password = request.POST['password']
     username = request.POST['username']
+    password = request.POST['password']
 
     if password == request.POST['confirm-password']:
       try:
@@ -41,3 +43,19 @@ def logout(request):
   if request.method == 'POST':
     auth.logout(request)
     return redirect('home')
+
+@login_required(login_url="/accounts/login")
+def view_profile(request, user_id):
+  args = {
+    'user': User.objects.get(pk=user_id)
+  }
+
+  return render(request, 'accounts/profile.html', args)
+
+@login_required(login_url="/accounts/login")
+def edit_profile(request, user_id):
+  args = {
+    'user': User.objects.get(pk=user_id)
+  }
+
+  return render(request, 'accounts/edit_profile.html', args)
